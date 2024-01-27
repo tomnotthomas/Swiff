12662651076ee89c4bc322c245c801ba819019c4
tomnotthomas <tom.schwabe1@outlook.de>
2024-01-27 15:39:46 +0100

    add shapes to the adtantages

--- react-website/src/Components/Advantages/Advantages.js
+++ react-website/src/Components/Advantages/Advantages.js
@@ -5,7 +5,21 @@ function Advantages() {
   return (
 
       <div className='advantages'>
-        advantages
+        <div className='advantages-left'>
+          <p>This is the left side</p>
+        </div>
+        <div className='advantages-right'>
+          <div className='info-box'>
+            <p>great stuff here. Amazing cloud gaming. Cloudy gaming experience wow.</p>
+          </div>
+          <div className='info-box'>
+            <p>great stuff here. Amazing cloud gaming. Cloudy gaming experience wow.</p>
+          </div>
+          <div className='info-box'>
+            <p>great stuff here. Amazing cloud gaming. Cloudy gaming experience wow.</p>
+          </div>
+          
+        </div>
       </div>
     
   )
