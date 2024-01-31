720f8383225872b0b086fc495d95d186ebc48c97
akubonin <akubonin@gmail.com>
2024-01-31 16:57:53 +0100

    merged new auth logic

--- Platform/server/router.ts
+++ Platform/server/router.ts
@@ -2,14 +2,13 @@ import { Router } from 'express'; // Import Router directly from express
 import passport from './config/passport-config.js';
 import session from './controllers/session-controller.js';
 import User from './models/user.js';
-import { createUser } from './controllers/userController.js'
+import { createUser, authUser } from './controllers/userController.js'
 const router = Router();
 
 
-
-router.get('/steamgames', session)
-
-router.post('/register', createUser)
+router.get('/steamgames', session);
+router.post('/register', createUser);
+router.post('/auth', authUser);
 
 
   // GET /auth/steam
