04b193482954c60540e4d27bae965cdd0c01c18c
tomnotthomas <tom.schwabe1@outlook.de>
2024-02-03 14:33:16 +0100

    add merge and changes on vmName creation

---
+++ Platform/server/dist/src/middlewares/auth.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+const jwtSecret = process.env.JWT_SECRET;
+export async function auth(req, res, next) {
+    try {
+        //   get the token from the authorization header
+        const token = await req.headers.authorization.split(" ")[1];
+        //check if the token matches the supposed origin
+        const decodedToken = await jwt.verify(token, jwtSecret);
+        // retrieve the user details of the logged in user
+        const user = await decodedToken;
+        // pass the user down to the endpoints here
+        req.user = user;
+        // pass down functionality to the endpoint
+        next();
+    }
+    catch (error) {
+        res.status(401).json({
+            error: new Error("Invalid request to auth!"),
+        });
+    }
+}
+;
+export async function authByToken(req, res, next) {
+    try {
+        //   get the token from the authorization header
+        const token = await req.cookies.TOKEN;
+        //check if the token matches the supposed origin
+        const decodedToken = await jwt.verify(token, jwtSecret);
+        // retrieve the user details of the logged in user
+        const user = await decodedToken;
+        // pass the user down to the endpoints here
+        req.user = user;
+        // pass down functionality to the endpoint
+        next();
+    }
+    catch (error) {
+        res.status(401).json({
+            error: new Error("Invalid request to auth Token!"),
+        });
+    }
+}
+;
---
+++ Platform/server/dist/src/models/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const DB_NAME = process.env.DATABASE_NAME;
+const DB_PORT = process.env.DATABASE_PORT;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}`);
+        console.log('MongoDB Connected');
+    }
+    catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
+    }
+};
+export default connectDB;
