6ca9f9e60351332d2d9d727b5155302ec299ef03
tomnotthomas <tom.schwabe1@outlook.de>
2024-02-03 14:19:01 +0100

    add vm name to user model

---
+++ Platform/server/src/helpers/instance-resource-name-generator.ts
@@ -0,0 +1,33 @@
+import User from '../models/User.js'
+
+
+
+ async function isVmNameUnique(vmName:string) {
+  try {
+    const existingVm = await User.findOne({ virtualMachine: vmName });
+    return !existingVm; // Returns true if the name is unique, false otherwise
+  } catch (error) {
+    console.error('Error checking uniqueness in the database:', error);
+    return false; // In case of an error, assume the name is not unique
+  }
+}
+export default async function generateVmName() {
+  const charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 15;
+  const prefix = 'vm';
+
+  let result: string;
+  do {
+    result = prefix;
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      result += charset.charAt(randomIndex);
+    }
+  } while (!(await isVmNameUnique(result)));
+  return result;
+}
+
+
+
+
+
