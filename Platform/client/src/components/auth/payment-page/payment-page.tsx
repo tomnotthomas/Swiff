0fe95c9d35bd2a396b34dda6bdaa85c7adb07486
tomnotthomas <tom.schwabe1@outlook.de>
2024-02-04 23:00:13 +0100

    change route from payment to login.

--- Platform/client/src/components/auth/payment-page/payment-page.tsx
+++ Platform/client/src/components/auth/payment-page/payment-page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './payment-page.css'
 import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 import { FaCcPaypal } from "react-icons/fa6";
 import { FaCcMastercard } from "react-icons/fa";
 import { SiAmericanexpress } from "react-icons/si";
@@ -11,9 +11,12 @@ import { FaCcJcb } from "react-icons/fa";
 
 function PaymentPage() {
   const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false); // Add a state variable for loading status
+
 
   const setPaymentStatusAndVM = () => {
-     fetch ('http://localhost:3001/setpaiduser', {
+    setLoading(true);  
+    fetch ('http://localhost:3001/setpaiduser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -23,15 +26,18 @@ function PaymentPage() {
           userEmail: 'tom@example.com'
       })
     })
-    .then(response => response.json())
+    .then(response => 
+      response.json())
     .then(data => {
       if (data.success) {
+        setLoading(false);
         // Use navigate for redirection
         navigate(data.redirectTo);
-        window.location.href = "/steam-login"
+        window.location.href = "/login"
       }
     })
     .catch(error => {
+      setLoading(false)
       console.error('There was a problem with the fetch operation:', error);
     });
   };
@@ -47,6 +53,7 @@ function PaymentPage() {
   <p> And enjoy 20h of high quality gaming per month</p>
   <div id='login-create-account'>
         <p className="small-notification">Continue without subscription</p>
+        {isLoading && <p>Your Gaming environment is currently being set up...</p>}
         <a className="small-notification" href='./'>Skip</a>
       </div>
   </div>
