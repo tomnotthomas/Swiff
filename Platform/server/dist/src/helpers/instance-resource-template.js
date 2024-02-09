069e3224df8fa394d84a5de88d8fb6a2130eaf82
tomnotthomas <tom.schwabe1@outlook.de>
2024-02-09 16:22:57 +0100

    change loading screen in payment page, add loginPage conditional links to next page, create instance resource template for gaming, delete baldurs gate image

--- Platform/server/dist/src/helpers/instance-resource-template.js
+++ Platform/server/dist/src/helpers/instance-resource-template.js
@@ -1,21 +1,45 @@
+export function instanceConfigurator(zone, name) {
+    // Check out the structure of an instance resource here: https://cloud.google.com/compute/docs/reference/rest/v1/instances
+    const instanceResource = {
+        "name": `${name}`,
+        "machineType": `zones/${zone}/machineTypes/n1-standard-8`, // Higher performance machine type
+        "sourceMachineImage": "projects/api-project-362391334008/global/machineImages/gamemachineimageparsec",
+        "guestAccelerators": [
+            {
+                "acceleratorType": `zones/${zone}/acceleratorTypes/nvidia-tesla-t4`, // T4 GPU
+                "acceleratorCount": 1
+            }
+        ],
+        "networkInterfaces": [
+            {
+                "network": "global/networks/default",
+                "accessConfigs": [
+                    {
+                        "name": "External NAT",
+                        "type": "ONE_TO_ONE_NAT"
+                    }
+                ]
+            }
+        ],
+        "scheduling": {
+            "onHostMaintenance": "TERMINATE", // Required for instances with GPUs
+            "automaticRestart": false, // Recommended for gaming workloads
+            "preemptible": false // Recommended for stable performance
+        }
+    };
+    return instanceResource;
+}
 //export function instanceConfigurator(zone: string, name: string): {} {
-//  // Check out the structure of an instance resource here: https://cloud.google.com/compute/docs/reference/rest/v1/instances
 //  const instanceResource = {
 //    "name": `${name}`,
-//    "machineType": `zones/${zone}/machineTypes/n1-standard-8`, // Higher performance machine type
+//    "machineType": `zones/${zone}/machineTypes/n2-standard-4`, // Adjust the machine type as needed
 //    "disks": [
 //      {
 //        "boot": true,
 //        "initializeParams": {
-//          "sourceImage": "projects/windows-cloud/global/images/family/windows-2022", // Windows Server 2022 image
-//          "diskSizeGb": "50" // Disk size 50 GB
+//          "sourceImage": "projects/windows-cloud/global/images/family/windows-2019",
+//          "diskSizeGb": "100" // Adjust the disk size as needed
 //        }
-//      }
-//    ],
-//    "guestAccelerators": [
-//      {
-//        "acceleratorType": `zones/${zone}/acceleratorTypes/nvidia-tesla-t4`, // T4 GPU
-//        "acceleratorCount": 1
 //      }
 //    ],
 //    "networkInterfaces": [
@@ -30,38 +54,10 @@
 //      }
 //    ],
 //    "scheduling": {
-//      "onHostMaintenance": "TERMINATE", // Required for instances with GPUs
-//      "automaticRestart": false, // Recommended for gaming workloads
-//      "preemptible": false // Recommended for stable performance
+//      "onHostMaintenance": "TERMINATE",
+//      "automaticRestart": false,
+//      "preemptible": false
 //    }
 //  }
 //  return instanceResource;
 //}
-export function instanceConfigurator(zone, name) {
-    //Check out the structure of an instance resource here:https://cloud.google.com/compute/docs/reference/rest/v1/instances
-    const instanceResource = {
-        "name": `${name}`,
-        "machineType": `zones/${zone}/machineTypes/e2-standard-2`,
-        "disks": [
-            {
-                "boot": true,
-                "initializeParams": {
-                    "sourceImage": "projects/debian-cloud/global/images/family/debian-10",
-                    "diskSizeGb": "10"
-                }
-            }
-        ],
-        "networkInterfaces": [
-            {
-                "network": "global/networks/default",
-                "accessConfigs": [
-                    {
-                        "name": "External NAT",
-                        "type": "ONE_TO_ONE_NAT"
-                    }
-                ]
-            }
-        ]
-    };
-    return instanceResource;
-}
